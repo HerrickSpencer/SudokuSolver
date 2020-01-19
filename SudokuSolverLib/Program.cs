3461aaf4887f4e36c5d96e6a375d9782292a88e8
Herrick Spencer <herricks@microsoft.com>
2020-01-18 20:45:47 -0800

    Convert library to Standard2.0 and update both apps to use the same library.

--- SudokuSolverLib/Program.cs
+++ SudokuSolverLib/Program.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.IO;
-using System.Text.RegularExpressions;
+﻿using SudokuSolverLib;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Diagnostics;
+using System.IO;
 
 namespace SudokuSolver
 {
